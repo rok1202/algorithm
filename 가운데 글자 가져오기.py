342eb6c51cd54bd5e2a754090b322204dfe623d1
Ethan Lim <hrlim1202@naver.com>
2021-07-12 10:21:40 +0900

    7/2

---
+++ 가운데 글자 가져오기.py
@@ -0,0 +1,11 @@
+def solution(s):
+    answer = ''
+    length=len(s)
+    if length % 2 == 0:
+        k = int(length/2)
+        answer =s[k-1]+ s[k]
+    else :
+        k = int(length/2)
+        answer = s[k]
+    return answer
+solution("abcde")
